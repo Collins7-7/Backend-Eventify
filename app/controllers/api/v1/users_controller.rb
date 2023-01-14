54d431147ed10b2c11e054b233d26e1c9cc60169
COllins <collinsmuiruri77@gmail.com>
2023-01-15 02:30:40 +0300

    updated user controller and serializer

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,17 +1,21 @@
 class Api::V1::UsersController < ApplicationController
-
-    def create 
-        user = User.new(user_params) 
+    # skip_before_action :authorized, only: [:create]
+    def create
+        user = User.new(user_params)
         if user.save
-             render json: user
-        else 
+            token = encode_token({user_id: user.id})
+            render json: {user: UserSerializer.new(user), jwt: token}
+        else
             render json: {error: "Error creating user"}
         end
-     end
+    end
+    def profile
+        render json: { user: UserSerializer.new(current_user) }, status: :accepted
+    end
 
-        private
-        
-        def user_params
-             params.require(:user).permit(:username, :email, :password)
-        end
+    private
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end
+    
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::API
+
+    def encode_token(payload) 
+        JWT.encode(payload, 'my_s3cr3t') 
+    end
 end
